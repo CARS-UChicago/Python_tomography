f2ed9dd3ed27278f33b45fecd908de00098a9b58
Mark Rivers <rivers@cars.uchicago.edu>
2023-01-26 17:57:57 -0600

    Add binning option; swapped suffix and extension meanings to be more standard

--- combine_vertical_stack.py
+++ combine_vertical_stack.py
@@ -3,6 +3,31 @@ import dxchange
 import netCDF4
 import numpy as np
 import argparse
+from numpy.lib.stride_tricks import as_strided
+
+# This code is taken from the https://github.com/sbrisard/rebin/blob/master/rebin.py code.
+# I just changed the last line to be able to specify that the dtype should be preserved.
+# It was returning float64.
+
+def rebin(a, factor, func=None):
+  a = np.asarray(a)
+  dim = a.ndim
+  if np.isscalar(factor):
+      factor = dim*(factor,)
+  elif len(factor) != dim:
+    raise ValueError('length of factor must be {} (was {})'
+                       .format(dim, len(factor)))
+  if func is None:
+    func = np.mean
+  for f in factor:
+    if f != int(f):
+      raise ValueError('factor must be an int or a tuple of ints '
+                       '(got {})'.format(f))
+
+  new_shape = [n//f for n, f in zip(a.shape, factor)]+list(factor)
+  new_strides = [s*f for s, f in zip(a.strides, factor)]+list(a.strides)
+  aa = as_strided(a, shape=new_shape, strides=new_strides)
+  return func(aa, axis=tuple(range(-dim, 0)),dtype=a.dtype)
 
 def read_tomo_volume(file):
   if file.endswith('h5'):
@@ -25,17 +50,22 @@ def write_tomo_volume(file, data):
     dxchange.write_netcdf4(data, fname=file, dname='VOLUME', overwrite=True)
 
 def combine_vertical_stack(base_file, num_files, pixel_overlap, 
-                           zstart=0, suffix='.h5',  
-                           extensions='A B C D E F G H I'):
+                           zstart=0, extension='.h5', binning=1,
+                           suffix='A B C D E F G H I'):
   logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
-  file = base_file + '_' + extensions[0] + 'recon' + suffix
-  extensions = extensions.split()
-  logging.info('Extensions = %s', extensions)
+  file = base_file + '_' + suffix[0] + 'recon' + extension
+  suffix = suffix.split()
+  logging.info('Suffixes = %s', suffix)
   logging.info('Reading file %s', file)
   v = read_tomo_volume(file)
+  if (binning != 1):
+    logging.info('Rebinning')
+    v = rebin(v, binning)
+    zstart = int(zstart/binning)
+    pixel_overlap = int(pixel_overlap/binning)
   nx = v.shape[2]
   ny = v.shape[1]
   nz = v.shape[0]
@@ -47,9 +77,12 @@ def combine_vertical_stack(base_file, num_files, pixel_overlap,
   logging.info('Inserting into volume array from slice %d to %d', 0, nz)
   vol[0:nz,:,:] = v
   for i in np.arange(1, num_files):
-    file = base_file + '_' + extensions[i] + 'recon' + suffix
+    file = base_file + '_' + suffix[i] + 'recon' + extension
     logging.info('Reading file %s', file)
     v = read_tomo_volume(file)
+    if (binning != 1):
+      logging.info('Rebinning')
+      v = rebin(v, binning)
     first_slice = (nz - pixel_overlap)*i
     last_slice = first_slice + nz
     if (zstart != 0):
@@ -58,10 +91,7 @@ def combine_vertical_stack(base_file, num_files, pixel_overlap,
       v = v[zstart:, :, :]
     logging.info('Inserting in volume array from slice %d to %d', first_slice, last_slice)
     vol[first_slice:last_slice, :, :] = v
-    #if (zstart ne 0) then begin
-    #    zrange = [zstart, nz-1]
-    #    vol[0, 0, (((nz-pixel_overlap)*i) + zstart)] = read_tomo_volume(base_file + '_' + extensions[i] + 'recon' + suffix, zrange=zrange)
-  file = base_file + 'combined_recon' + suffix
+  file = base_file + 'combined_recon' + extension
   logging.info('Writing combined volume %s', file)
   write_tomo_volume(file, vol)
   logging.info('Done')
@@ -76,12 +106,14 @@ if __name__ == "__main__":
                     help="number of overlappped pixels")
   parser.add_argument("-z", "--zstart", type=int, default=0,
                     help="first z value in second and subsequent files (default=0)")
-  parser.add_argument("-s", "--suffix", default='.h5',
-                    help="file suffix (default='.h5')")
-  parser.add_argument("-e", "--extensions", default='A B C D E F G H I',
-                    help="file extensions (default='A B C D E F G H I')")
+  parser.add_argument("-b", "--binning", type=int, default=1,
+                    help="binning for output (default=1, no binning)")
+  parser.add_argument("-e", "--extension", default='.h5',
+                    help="file extension (default='.h5')")
+  parser.add_argument("-s", "--suffix", default='A B C D E F G H I',
+                    help="file suffix (default='A B C D E F G H I')")
   args = parser.parse_args()
   combine_vertical_stack(args.base_file, args.num_files, 
-      args.pixel_overlap, zstart=args.zstart, 
-      suffix=args.suffix, extensions=args.extensions) 
+      args.pixel_overlap, zstart=args.zstart, binning=args.binning,
+      suffix=args.suffix, extension=args.extension) 
  
